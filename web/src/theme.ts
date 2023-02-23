dd1ea7618330d0343d397e0b4e3f235725ca96ee
7wataaa <nn.contact.n@gmail.com>
2023-02-23 21:10:59 +0900

    [add] chakra uiをセットアップ

---
+++ web/src/theme.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: 'black.100',
+        color: 'gray.800',
+      },
+    },
+  },
+})
